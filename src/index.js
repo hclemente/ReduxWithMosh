6b4c77cce96464a562c6ffded0ea0ebba05c6c16
Harry Clemente <hclemente1989@gmail.com>
2021-07-10 12:15:29 -0700

    Up to functional programming

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { compose, pipe} from 'lodash/fp';
+
+let input = "  Javascript  ";
+let output = "<div>" + input.trim() + "</div";
+
+const trim = str => str.trim();
+//const wrapInDiv = str => `<div>${str}</div>`;
+const wrap = type => str => `<${type}>${str}</${type}>`;
+const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap("div"));
+
+console.log(transform(input))
+
+//const result = wrapInDiv(toLowerCase(trim(input)));
+
+
+
+
